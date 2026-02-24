7789c0f649faa1afcffcb9a6ed4793a9577e45ee
ANNNAAAAAA12 <aramos598@unab.edu.co>
2026-02-24 09:39:34 -0500

    primer commit

---
+++ conexión_activa.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class conexión_activa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class conexión_activa
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class conexión_activa
+     */
+    public conexión_activa()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ usario.java
@@ -0,0 +1,11 @@
+
+public class usario
+{
+    public String nombre;
+    public String correo;
+    public String apellido; 
+    
+    public void getName(){}
+    public void getcorreo(){}
+    public void getapellido(){}
+}
